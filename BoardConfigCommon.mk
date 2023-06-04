511096b82549158e0f747b2185c391c9a1eccc8b
Devang Chaudhary <devangsingh369@gmail.com>
2023-06-04 11:06:24 +0530

    sm8250-common: Switch to `zyc` Clang
    Signed-off-by: Devang Chaudhary <devangsingh369@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,8 +122,8 @@ TARGET_KERNEL_ADDITIONAL_FLAGS += AR=llvm-ar LLVM=1 LLVM_IAS=1 NM=llvm-nm OBJCOP
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
-TARGET_KERNEL_CLANG_VERSION := neutron
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-neutron
+TARGET_KERNEL_CLANG_VERSION := zyc
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-zyc
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true

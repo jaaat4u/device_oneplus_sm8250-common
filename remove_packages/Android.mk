a47fdd8dc30a37f10d4843fdb49a55d85a626f22
Devang Chaudhary <devangsingh369@gmail.com>
2023-06-12 08:18:01 +0530

    sm8250-common: Ship with `Aperture Camera` App
    Signed-off-by: Devang Chaudhary <devangsingh369@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX MusicFX Aperture Gallery Gallery2 DevCamera Car Drive
+LOCAL_OVERRIDES_PACKAGES := AudioFX MusicFX Gallery Gallery2 DevCamera Car Drive
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
